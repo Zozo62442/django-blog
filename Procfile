71d1d2704686010d6277644cd9164a3e2ec6a26b
Zozo62442 <zoe.rotterr@gmail.com>
2025-06-27 00:39:20 +0800

    Prepare project for Heroku deployment

--- Procfile
+++ Procfile
@@ -10,3 +10,5 @@ pathspec==0.12.1
 platformdirs==4.3.8
 setuptools==80.9.0
 sqlparse==0.5.3
+gunicorn~=20.1
+web: gunicorn codestar.wsgi:application
--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0m-kc-qzs=5@xp_fec&ktm6$87(sebq^z3eq=-!erqc0ehj!)g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
