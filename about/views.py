5254e299755a545bcdd86a02c285d03a1830cc9c
Zozo62442 <zoe.rotterr@gmail.com>
2025-07-02 20:54:11 +0800

    enable serving of image files

--- about/views.py
+++ about/views.py
@@ -4,6 +4,11 @@ from .forms import CollaborateForm
 from django.contrib import messages
 
 def about_me(request):
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
     """
     Renders the About page
     """
